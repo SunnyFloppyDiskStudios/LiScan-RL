599d9948889591bcbee641549cf1825f1ace4305
SunnyFlops <arav.prasad@outlook.com>
2025-06-18 09:40:12 +1200

    update spatial mesh collider
    untested

--- Assets/Assets/Scripts/MeshMaker/SpatialMeshColliderUpdater.cs
+++ Assets/Assets/Scripts/MeshMaker/SpatialMeshColliderUpdater.cs
@@ -1,61 +1,24 @@
-using System.Collections.Generic;
 using UnityEngine;
-using Oculus.Interaction;
 
 public class SpatialMeshColliderUpdater : MonoBehaviour {
-    private MeshCollider combinedCollider;
-    private Mesh combinedMesh;
+    private MeshCollider meshCollider;
+    private RoomMeshAnchor roomMeshAnchor;
+    private Mesh lastMesh;
 
     void Start() {
-        combinedCollider = gameObject.AddComponent<MeshCollider>();
-        combinedMesh = new Mesh();
-
-        BuildSceneMeshCollider();
-    }
-    
-    public class SceneCaptureTrigger : MonoBehaviour {
-        public OVRSceneManager sceneManager;
-
-        void Update() {
-            if (Input.GetKeyDown(KeyCode.Space)) {
-                sceneManager.RequestSceneCapture();
-            }
-        }
+        meshCollider = GetComponent<MeshCollider>();
+        roomMeshAnchor = FindObjectOfType<RoomMeshAnchor>();
     }
 
-    void BuildSceneMeshCollider() {
-        var sceneMeshes = FindObjectsOfType<OVRScenePlaneMeshFilter>();
+    void Update() {
+        if (roomMeshAnchor == null) return;
 
-        var vertices = new List<Vector3>();
-        var triangles = new List<int>();
-        int vertexOffset = 0;
+        Mesh currentMesh = roomMeshAnchor.GetComponent<MeshFilter>().sharedMesh;
 
-        foreach (var plane in sceneMeshes) {
-            Mesh mesh = plane.GetComponent<MeshFilter>().sharedMesh;
-            if (mesh == null) continue;
-
-            var verts = mesh.vertices;
-            var tris = mesh.triangles;
-
-            for (int i = 0; i < verts.Length; i++) {
-                Vector3 worldVert = plane.transform.TransformPoint(verts[i]);
-                vertices.Add(transform.InverseTransformPoint(worldVert));
-            }
-
-            for (int i = 0; i < tris.Length; i++) {
-                triangles.Add(tris[i] + vertexOffset);
-            }
-
-            vertexOffset += verts.Length;
-        }
-
-        if (vertices.Count > 0) {
-            combinedMesh.Clear();
-            combinedMesh.SetVertices(vertices);
-            combinedMesh.SetTriangles(triangles, 0);
-            combinedMesh.RecalculateNormals();
-
-            combinedCollider.sharedMesh = combinedMesh;
+        if (currentMesh != null && currentMesh != lastMesh) {
+            meshCollider.sharedMesh = null;
+            meshCollider.sharedMesh = currentMesh;
+            lastMesh = currentMesh;
         }
     }
 }
