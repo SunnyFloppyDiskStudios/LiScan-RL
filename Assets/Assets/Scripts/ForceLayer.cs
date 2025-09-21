a5544b2ba37d8de45929a98d3a183c604c4824a2
SunnyFlops <arav.prasad@outlook.com>
2025-09-21 19:07:15 +1200

    add wall mesh collider, update ide plugin

--- Assets/Assets/Scripts/ForceLayer.cs
+++ Assets/Assets/Scripts/ForceLayer.cs
@@ -5,7 +5,7 @@ public class ForceLayer : MonoBehaviour {
 
     void Update() {
         foreach (GameObject go in FindObjectsOfType<GameObject>()) {
-            if (go.layer != 7 && go.layer != 2) {
+            if (go.layer != 7 && go.layer != 8) {
                 SetLayerRecursively(go, newLayer);
             }
         }
@@ -13,7 +13,17 @@ public class ForceLayer : MonoBehaviour {
 
     void SetLayerRecursively(GameObject obj, int layer) {
         if (obj.layer == layer) return;
+
         obj.layer = layer;
+
+        MeshFilter mf = obj.GetComponent<MeshFilter>();
+        if (mf is not null && mf.sharedMesh is not null) {
+            MeshCollider mc = obj.GetComponent<MeshCollider>();
+            if (mc is null) mc = obj.AddComponent<MeshCollider>();
+            mc.sharedMesh = mf.sharedMesh;
+            mc.convex = true;
+        }
+
         foreach (Transform child in obj.transform) {
             SetLayerRecursively(child.gameObject, layer);
         }
